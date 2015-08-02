1b25666e840fd65ec093f8fcee473c96e6b4449f
suresh <venkatasureshdv@gmail.com>
2015-08-02 15:51:26 +0530

    ADDED KIRAN AS COMMENT

--- src/com/suresh/emergencylocationidentifier/BootBroadcastReceiver.java
+++ src/com/suresh/emergencylocationidentifier/BootBroadcastReceiver.java
@@ -14,6 +14,7 @@ public class BootBroadcastReceiver extends BroadcastReceiver {
         	if(utils.isEmergencyServiceRunnig()){
         		  Intent serviceIntent = new Intent(context, Location_Msg_Shake.class);       
                   context.startService(serviceIntent);
+//KIRAN
         	}
         	
             //Service    
